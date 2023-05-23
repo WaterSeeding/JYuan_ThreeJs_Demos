e431af69feea344daedb5fbbd0b77947f866fde9
JYuan <31184164+WaterSeeding@users.noreply.github.com>
2023-05-23 16:23:28 +0800

    终于理解了selective-bloom的概念

--- src/pages/BloomDemo/index.tsx
+++ src/pages/BloomDemo/index.tsx
@@ -7,6 +7,17 @@ import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 
+/**
+ * 在我看来，这个官方的例子过于复杂了。但selective bloom的概念本身很简单:
+ * 1. 使所有non-bloomed的Object3D materials/colors变得全黑
+ * 2. 使用bloomComposer（效果合成器）渲染场景
+ * 3. 恢复materials/colors原状
+ * 4. 用finalComposer（效果合成器）渲染场景
+ * 
+ * 剩下的就是:
+ * 如何管理变暗/变黑 non-bloomed Object3D的materials/colors和恢复Object3D的materials/colors原状，这取决于自己的代码了。
+ */
+
 const Earth = () => {
   const [isInitScene, setIsInitScene] = useState<boolean>(false);
 
@@ -37,14 +48,54 @@ const Earth = () => {
 
     let controls = new OrbitControls(camera, renderer.domElement);
 
-    let light = new THREE.DirectionalLight(0xffffff, 0.5);
-    light.position.setScalar(1);
-    scene.add(light, new THREE.AmbientLight(0xffffff, 0.5));
+    let light = new THREE.DirectionalLight(0xffffff, 1.0);
+    light.position.setScalar(10);
+    scene.add(light, new THREE.AmbientLight(0xffffff, 1.0));
 
     let uniforms = {
       globalBloom: { value: 1 },
     };
 
+    let box = addBox(scene, uniforms);
+    console.log("box", box);
+    addTarget(scene, uniforms);
+
+    let { bloomComposer,
+      finalComposer } = addComposer(scene, camera, renderer);
+
+    renderer.setAnimationLoop((_) => {
+      renderer.setClearColor(0x000000);
+      uniforms.globalBloom.value = 1;
+
+      if (box) {
+        box.material.color.set(0x000000);
+      }
+      bloomComposer.render();
+
+      renderer.setClearColor(0x000000);
+      uniforms.globalBloom.value = 0;
+
+      if (box) {
+        box.material.color.set(0x00ff00);
+      }
+      finalComposer.render();
+    });
+
+    window.onresize = function () {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(width, height);
+
+      bloomComposer.setSize(width, height);
+      finalComposer.setSize(width, height);
+    };
+  };
+
+  const addTarget = (scene: THREE.Scene, uniforms: any) => {
     // texture
     new THREE.TextureLoader().load(
       require('./img/hardwood2_diffuse.jpg'),
@@ -57,11 +108,6 @@ const Earth = () => {
         let ctx = c.getContext('2d');
         ctx!.drawImage(img, 0, 0);
 
-        let c2 = document.createElement('canvas');
-        c2.width = c2.height = min;
-        let ctx2 = c2.getContext('2d');
-        ctx2!.clearRect(0, 0, min, min);
-
         ['#f00', '#0f0', '#ff0', '#f0f', '#0ff'].forEach((col, i, a) => {
           let id = i - (a.length - 1) / 2;
           let dist = id * 150;
@@ -72,18 +118,22 @@ const Earth = () => {
         });
 
         let cTex = new THREE.CanvasTexture(c);
+
+        let c2 = document.createElement('canvas');
+        c2.width = c2.height = min;
+        let ctx2 = c2.getContext('2d');
+        ctx2!.clearRect(0, 0, min, min);
+
         let c2Tex = new THREE.CanvasTexture(c2);
 
-        setInterval(() => {
-          ctx2!.clearRect(0, 0, min, min);
-          let id = THREE.MathUtils.randInt(0, 4) - 2;
-          let dist = id * 150;
-          ctx2!.beginPath();
-          ctx2!.arc(min * 0.5 + dist, min * 0.5, 25, 0, 2 * Math.PI);
-          ctx2!.fillStyle = '#fff';
-          ctx2!.fill();
-          c2Tex.needsUpdate = true;
-        }, 125);
+        ctx2!.clearRect(0, 0, min, min);
+        let id = -2;
+        let dist = id * 150;
+        ctx2!.beginPath();
+        ctx2!.arc(min * 0.5 + dist, min * 0.5, 25, 0, 2 * Math.PI);
+        ctx2!.fillStyle = '#fff';
+        ctx2!.fill();
+        c2Tex.needsUpdate = true;
 
         let g = new THREE.PlaneGeometry(5, 5);
         g.rotateX(Math.PI * -0.5);
@@ -131,30 +181,83 @@ const Earth = () => {
         scene.add(o);
       },
     );
+  }
 
-    window.onresize = function () {
-      const width = window.innerWidth;
-      const height = window.innerHeight;
+  const addBox = (scene: THREE.Scene, uniforms: any) => {
+    // texture
+    let map = new THREE.TextureLoader().load(
+      require('./img/map.png')
+    );
+    let emissiveMap = new THREE.TextureLoader().load(
+      require('./img/emissiveMap.png')
+    );
 
-      camera.aspect = width / height;
-      camera.updateProjectionMatrix();
+    let g = new THREE.BoxGeometry(1, 1, 1);
+    g.rotateX(Math.PI * -0.5);
+    let m = new THREE.MeshStandardMaterial({
+      roughness: 0.6,
+      metalness: 0.5,
+      map: map,
+      emissiveMap: emissiveMap,
+      // @ts-ignore;
+      onBeforeCompile: (shader) => {
+        shader.vertexShader = shader.vertexShader.replace(
+          'varying vec3 vViewPosition;',
+          `
+          varying vec3 vViewPosition;
+          // Varyings类型变量作为顶点着色器中确定的值
+          varying vec2 vUV;
+          `,
+        );
+        shader.vertexShader = shader.vertexShader.replace(
+          '#include <fog_vertex>',
+          `
+          #include <fog_vertex>
+          vUV = uv;
+          `,
+        );
+        shader.uniforms.globalBloom = uniforms.globalBloom;
+        // varying vec3 vViewPosition;
+        shader.fragmentShader = `
+        uniform float globalBloom;
+        varying vec2 vUV;
+          ${shader.fragmentShader}
+        `.replace(
+          `#include <dithering_fragment>`,
+          `#include <dithering_fragment>
+            vec3 col = texture2D( map, vUV).rgb;
+            float em = texture2D( emissiveMap, vUV ).g;
+            col *= em;
+            gl_FragColor.rgb = mix(gl_FragColor.rgb, col, globalBloom);
 
-      renderer.setSize(width, height);
+        `,
+        );
+      },
+    });
+    let o = new THREE.Mesh(g, m);
+    o.position.setX(1)
+    o.position.setY(1)
+    scene.add(o);
 
-      bloomComposer.setSize(width, height);
-      finalComposer.setSize(width, height);
-    };
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    cube.position.setX(-1)
+    cube.position.setY(1)
+    scene.add(cube);
 
-    // bloom
+    return cube
+  }
+
+  const addComposer = (scene: THREE.Scene, camera: THREE.Camera, renderer: THREE.WebGLRenderer) => {
     const renderScene = new RenderPass(scene, camera);
-
     const bloomPass = new UnrealBloomPass(
       new THREE.Vector2(window.innerWidth, window.innerHeight),
       3.0,
       0.1,
       0.1,
     );
-
     const bloomComposer = new EffectComposer(renderer);
     bloomComposer.renderToScreen = false;
     bloomComposer.addPass(renderScene);
@@ -190,20 +293,11 @@ const Earth = () => {
     const finalComposer = new EffectComposer(renderer);
     finalComposer.addPass(renderScene);
     finalComposer.addPass(finalPass);
-
-    renderer.setAnimationLoop((_) => {
-      renderer.setClearColor(0x000000);
-      uniforms.globalBloom.value = 1;
-
-      bloomComposer.render();
-
-      renderer.setClearColor(0x202020);
-      uniforms.globalBloom.value = 0;
-
-      finalComposer.render();
-      //renderer.render(scene, camera);
-    });
-  };
+    return {
+      bloomComposer,
+      finalComposer
+    }
+  }
 
   return (
     <div className={styles.container}>
